feca6d298c6d7b42acc51cf953104bd08b498a9e
Ariadani Dwi Fathurahman <ariadanidf@gmail.com>
2023-06-12 12:14:44 +0700

    probis 2-3

---
+++ app/Models/Price.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Price extends Model
+{
+    use HasFactory;
+    protected $fillable = ['name', 'harga_barang', 'harga_ongkir', 'harga_total'];
+}
