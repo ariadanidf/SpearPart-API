da5a9d394658f9ec29e1c262a7f8363c5f619684
Ariadani Dwi Fathurahman <ariadanidf@gmail.com>
2023-06-12 14:46:29 +0700

    fix

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -6,59 +6,59 @@ use App\Models\Order;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
+use Exception;
 use Illuminate\Support\Facades\Http;
 
 class OrderController extends Controller
 {
     //bikin order
     public function ordering(Request $request)
-{
-    try {
-        $request->validate([
-            'id_customer' => 'required',
-            'nama_barang' => 'required',
-            'alamat_penerima' => 'required',
-            'order_date' => 'required',
-            'jenis_pengiriman' => 'required',
-            'berat_barang' => 'required',
-            'harga_barang' => 'required'
-        ]);
-
-        $order = Order::create([
-            'id_order' => $request->id_order,
-            'id_customer' => $request->id_customer,
-            'nama_barang' => $request->nama_barang,
-            'alamat_penerima' => $request->alamat_penerima,
-            'order_date' => $request->order_date,
-            'jenis_pengiriman' => $request->jenis_pengiriman,
-            'berat_barang' => $request->berat_barang,
-            'harga_barang' => $request->harga_barang
-        ]);
-
-        if ($order) {
-            $orderData = $order->toArray();
-            $orderData['id_order'] = $orderData['id'];
-            unset($orderData['id']);
-            
-            return response()->json([
-                'code' => 200,
-                'message' => 'Sukses',
-                'data' => $orderData
+    {
+        try {
+            $request->validate([
+                'id_customer' => 'required',
+                'nama_barang' => 'required',
+                'alamat_penerima' => 'required',
+                'order_date' => 'required',
+                'jenis_pengiriman' => 'required',
+                'berat_barang' => 'required',
+                'harga_barang' => 'required'
             ]);
-        } else {
+    
+            $order = Order::create([
+                'id_order' => $request->id_order,
+                'id_customer' => $request->id_customer,
+                'nama_barang' => $request->nama_barang,
+                'alamat_penerima' => $request->alamat_penerima,
+                'order_date' => $request->order_date,
+                'jenis_pengiriman' => $request->jenis_pengiriman,
+                'berat_barang' => $request->berat_barang,
+                'harga_barang' => $request->harga_barang
+            ]);
+    
+            if ($order) {
+                $orderData = $order->toArray();
+                $orderData['id_order'] = isset($orderData['id']) ? $orderData['id'] : null;
+                unset($orderData['id']);
+                
+                return response()->json([
+                    'code' => 200,
+                    'message' => 'Sukses',
+                    'data' => $orderData
+                ]);
+            } else {
+                return response()->json([
+                    'code' => 404,
+                    'message' => 'Kesalahan Server Internal'
+                ]);
+            }
+        } catch (Exception $error) {
             return response()->json([
-                'code' => 404,
-                'message' => 'Kesalahan Server Internal'
+                'code' => 500,
+                'message' => 'Kesalahan Server Internal: ' . $error->getMessage()
             ]);
         }
-    } catch (Exception $error) {
-        return response()->json([
-            'code' => 500,
-            'message' => 'Kesalahan Server Internal'
-        ]);
     }
-
-}
 
     //update order
     public function updateOrder(Request $request)
